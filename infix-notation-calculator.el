440283f8bf7a422f3c8d2211a1efbf4d8b6b59aa
Christian Johansson <christian@cvj.se>
2021-08-14 23:05:31 +0200

    Implemented evaluate selected region and added tests for it

--- infix-notation-calculator.el
+++ infix-notation-calculator.el
@@ -7,178 +7,46 @@
 ;;; Code:
 
 
+(require 'infix-notation-calculator-parser)
+
 ;; TODO Create mode here based on fundamental mode
 
 (defun infix-notation-calculator-on-selected-region ()
   "Calculate results of selected region."
   (interactive)
-  ;; TODO Implement this
-  )
+  (let ((selected-text (buffer-substring-no-properties (mark) (point))))
+    (setq
+     selected-text
+     (infix-notation-calculator--adjust-string
+      selected-text))
+    (infix-notation-calculator--translate-string
+     selected-text)))
+
+(defun infix-notation-calculator--translate-string (string)
+  "Translate STRING, return value."
+  (unless (get-buffer "*infix-notation-calculator-buffer*")
+    (generate-new-buffer "*infix-notation-calculator-buffer*"))
+  (switch-to-buffer "*infix-notation-calculator-buffer*")
+  (kill-region (point-min) (point-max))
+  (insert string)
+  (let ((translation (infix-notation-calculator-parser-translate)))
+    (kill-buffer)
+    translation))
+
+(defun infix-notation-calculator--adjust-string (string)
+  "Adjust STRING for parser."
+  ;; Remove trailing equals sign
+  (when (string-match-p "^.+=$" string)
+    (setq string (substring string 0 -1)))
+
+  ;; Add trailing newline if missing
+  (unless (string-match-p "^.+\n$" string)
+    (setq string (concat string "
+")))
+
+  string)
 
 
-(defun infix-notation-calculator-generate-parser ()
-  "Generate parser for plug-in."
-  (if (and
-       (boundp 'parser-generator--e-identifier)
-       (fboundp 'parser-generator-set-look-ahead-number)
-       (boundp 'parser-generator--global-attributes)
-       (boundp 'parser-generator--context-sensitive-attributes)
-       (boundp 'parser-generator-lr--global-precedence-attributes)
-       (boundp 'parser-generator-lr--context-sensitive-precedence-attribute)
-       (boundp 'parser-generator--global-declaration)
-       (boundp 'parser-generator-lr--precedence-comparison-function)
-       (fboundp 'parser-generator-set-grammar)
-       (boundp 'parser-generator-lex-analyzer--function)
-       (boundp 'parser-generator-lex-analyzer--get-function)
-       (fboundp 'parser-generator-lr-export-to-elisp)
-       )
-      (progn
-
-        (setq
-         parser-generator--e-identifier
-         '%empty)
-        (parser-generator-set-look-ahead-number 1)
-        (setq
-         parser-generator--global-attributes
-         '(%left %precedence %right))
-        (setq
-         parser-generator--context-sensitive-attributes
-         '(%prec))
-        (setq
-         parser-generator-lr--global-precedence-attributes
-         '(%left %precedence %right))
-        (setq
-         parser-generator-lr--context-sensitive-precedence-attribute
-         '%prec)
-        (setq
-         parser-generator--global-declaration
-         '(
-           (%left "-" "+")
-           (%left "*" "/")
-           (%precedence NEG)
-           (%right "^")))
-        (setq
-         parser-generator-lr--precedence-comparison-function
-         (lambda(a-type a-value _b-type b-value)
-           (cond
-
-            ((and
-              a-value
-              b-value)
-             (cond
-              ((> a-value b-value)
-               t)
-
-              ((< a-value b-value)
-               nil)
-
-              ((= a-value b-value)
-
-               (cond
-                ((equal a-type '%left)
-                 t)
-
-                ((equal a-type '%right)
-                 nil)
-
-                ((equal a-type '%precedence)
-                 t))
-
-               )))
-
-            ((and
-              a-value
-              (not b-value))
-             t)
-
-            ((and
-              (not a-value)
-              (not b-value))
-             nil)
-
-            )))
-
-        (parser-generator-set-grammar
-         '(
-           (start input line exp)
-           ("+" "-" "*" "/" "^" "(" ")" "\n" NUM)
-           (
-            (start input)
-            (input
-             %empty
-             (input line (lambda(args) (nth 1 args))))
-            (line
-             "\n"
-             (exp "\n" (lambda(args) (nth 0 args))))
-            (exp
-             NUM
-             (exp "+" exp (lambda(args) (+ (float (nth 0 args)) (nth 2 args))))
-             (exp "-" exp (lambda(args) (- (float (nth 0 args)) (nth 2 args))))
-             (exp "*" exp (lambda(args) (* (float (nth 0 args)) (nth 2 args))))
-             (exp "/" exp (lambda(args) (/ (float (nth 0 args)) (nth 2 args))))
-             ("-" exp %prec NEG (lambda(args) (- (float (nth 1 args)))))
-             (exp "^" exp (lambda(args) (expt (float (nth 0 args)) (nth 2 args))))
-             ("(" exp ")" (lambda(args) (nth 1 args)))))
-           start))
-
-        (setq
-         parser-generator-lex-analyzer--function
-         (lambda (index)
-           (with-current-buffer "*buffer*"
-             (let ((token))
-               (when
-                   (<
-                    index
-                    (point-max))
-                 (goto-char
-                  index)
-
-                 ;; Skip white-space(s)
-                 (when (looking-at-p "[\t ]+")
-                   (when
-                       (search-forward-regexp "[^\t ]" nil t)
-                     (forward-char -1)))
-
-                 (cond
-                  ((looking-at "\\([0-9]+\\.[0-9]+\\|[0-9]+\\)")
-                   (setq
-                    token
-                    `(NUM ,(match-beginning 0) . ,(match-end 0))))
-                  ((looking-at "\\(\\+\\|-\\|*\\|/\\|\\^\\|)\\|(\\|\n\\)")
-                   (let ((symbol
-                          (buffer-substring-no-properties
-                           (match-beginning 0)
-                           (match-end 0))))
-                     (setq
-                      token
-                      `(,symbol ,(match-beginning 0) . ,(match-end 0)))))
-                  (t (error "Unexpected input at %d!" index))))
-               token))))
-
-        (setq
-         parser-generator-lex-analyzer--get-function
-         (lambda (token)
-           (with-current-buffer "*buffer*"
-             (let ((start (car (cdr token)))
-                   (end (cdr (cdr token))))
-               (when (<= end (point-max))
-                 (let ((symbol
-                        (buffer-substring-no-properties start end)))
-                   (when
-                       (string-match-p "^\\([0-9]+\\.[0-9]+\\|[0-9]+\\)$" symbol)
-                     (setq
-                      symbol
-                      (string-to-number symbol)))
-                   symbol))))))
-
-        (let ((export (parser-generator-lr-export-to-elisp "infix-notation-calculator-parser")))
-          (with-temp-buffer
-            (insert export)
-            (write-file "infix-notation-calculator-parser.el")
-            (kill-buffer))
-          )
-        (message "Parser Generator at infix-notation-calculator-parser.el"))
-       (error "Parser Generator is not available!")))
-
+(provide 'infix-notation-calculator)
 
 ;;; infix-notation-calculator.el ends here
--- test/infix-notation-calculator-test.el
+++ test/infix-notation-calculator-test.el
@@ -7,14 +7,35 @@
 ;;; Code:
 
 
+(require 'infix-notation-calculator)
 (require 'ert)
 
 (defun infix-notation-calculator-test ()
   "Test mode and interactive functions."
 
-  (should (equal t t))
+  (should
+   (equal
+    "abc\n"
+    (infix-notation-calculator--adjust-string "abc=")))
+  (should
+   (equal
+    "abc\n"
+    (infix-notation-calculator--adjust-string "abc\n")))
 
-  )
+  (should
+   (equal
+    3.0
+    (infix-notation-calculator--translate-string "-5+8\n")))
+  (should
+   (equal
+    14.0
+    (infix-notation-calculator--translate-string "2+3*4\n")))
+  (should
+   (equal
+    10.0
+    (infix-notation-calculator--translate-string "2*3+4\n"))))
 
+
+(provide 'infix-notation-calculator-test)
 
 ;;; infix-notation-calculator-test.el ends here
